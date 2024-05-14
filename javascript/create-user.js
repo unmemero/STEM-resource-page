9ac551a64307339b09f0f8ba3c9ec34dd3fa8fad
Rafael Garcia <fafagm99@gmail.com>
2024-05-13 21:41:52 -0600

    Added create user page

--- javascript/create-user.js
+++ javascript/create-user.js
@@ -1,6 +1,34 @@
-const firstName = document.getElementById('userFirstName').value;
-const lastName = document.getElementById('userLastName').value;
-const email = document.getElementById('userEmail').value;
-const password = document.getElementById('userPassword').value;
-const passwordConfirmation = document.getElementById('userPasswordConfirm').value;
+const firstName = document.getElementById('userFirstName');
+const lastName = document.getElementById('userLastName');
+const email = document.getElementById('userEmail');
+const password = document.getElementById('userPassword');
+const passwordConfirmation = document.getElementById('userPasswordConfirm');
+const submitButton = document.getElementById('submitButton');
+submitButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+    console.log(firstName.value,lastName.value,email.value,password.value,passwordConfirmation.value);
+});
 
+const passwordToggle = document.getElementById('password-toggle');
+passwordToggle.addEventListener('click',(e) => {
+    e.preventDefault();
+    if(password.type == "password"){
+        password.type = "text";
+        password.innerHTML = "<span class=\"material-symbols-outlined\">visibility_off</span>";
+    }else{
+        password.type = "password";
+        password.innerHTML = "<span class=\"material-symbols-outlined\">visibility</span>";
+    }
+});
+
+const confirmToggle = document.getElementById('confirm-toggle');
+confirmToggle.addEventListener("click",(e)=>{
+    e.preventDefault();
+    if(passwordConfirmation.type == "password"){
+        passwordConfirmation.type = "text";
+        passwordConfirmation.innerHTML = "<span class=\"material-symbols-outlined\">visibility_off</span>";
+    }else{
+        passwordConfirmation.type = "password";
+        passwordConfirmation.innerHTML = "<span class=\"material-symbols-outlined\">visibility</span>";
+    }
+});
--- javascript/login.js
+++ javascript/login.js
@@ -3,6 +3,7 @@ const passwordInput = document.getElementById("userPassword");
 const submitButton = document.getElementById("submitButton");
 const emailText = emailInput.value;
 const passwordText = passwordInput.value;
+const passwordToggle = document.getElementById("password-toggle");
 
 emailInput.addEventListener("mouseover", ()=> {
     emailInput.focus();
@@ -10,4 +11,16 @@ emailInput.addEventListener("mouseover", ()=> {
 
 passwordInput.addEventListener("mouseover", ()=> {
     passwordInput.focus();
+});
+
+passwordToggle.addEventListener("click", (e)=>{
+    e.preventDefault();
+    if(passwordInput.type=="password"){
+        passwordInput.type="text"
+        passwordToggle.innerHTML = "<span class=\"material-symbols-outlined\">visibility_off</span>";
+    }
+    else{ 
+        passwordInput.type="password";
+        passwordToggle.innerHTML = "<span class=\"material-symbols-outlined\">visibility</span>";
+    }
 });
